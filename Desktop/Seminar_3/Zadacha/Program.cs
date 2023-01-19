d48d0247d41372ad133977c54356bb3eea8e51ed
Arteeex <arteeex187@gmail.com>
2023-01-19 17:19:11 +0700

    ready

---
+++ Desktop/Seminar_3/Zadacha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+string str = Console.ReadLine();
+if(str[0] == str[4] && str[1] == str[3])
+{
+    Console.WriteLine("DA");
+}
+else
+{
+    System.Console.WriteLine("net");
+}
+return;
---
+++ Desktop/Seminar_3/zadacha_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+for (int i = 1; i<a+1; i++)
+{
+    count = Convert.ToInt32(Math.Pow(i, 3));
+    System.Console.WriteLine(count);
+}
